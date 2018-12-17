090c0942d0ee53048225576a76345b375ed98a5b
ManhMoc <38889338+ManhMoc@users.noreply.github.com>
2018-12-17 23:36:18 +0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Net1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Net1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.WelComeString = "Chao mung ban den voi ViewBag";
+            var mes = new MessageModel();
+            mes.Welcome = "Chao mung den boi Model";
+            return View(mes);
+        }
+    }
+}
